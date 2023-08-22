610bfcd69fe515ea454650741d67f24f49a82e5b
gtxzsxxk <2524395907@qq.com>
2023-08-22 19:35:53 +0800

    feat: add some ac code comment

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@
  * ./thulogin username
  * ./thulogin --username username --pwd pwd
  * ./thulogin --specify-server url --specify-ua user-agent --specify-ac-id ac_id
+ *
+ * ac_id:
+ *  ethernet: 173
+ *  tsinghua-5g tsinghua: 135
  * */
 int main(int argc, char **argv) {
     AuthWorker au("http://auth4.tsinghua.edu.cn", "135");
@@ -20,9 +24,11 @@ int main(int argc, char **argv) {
         std::cin >> user;
 
         pwd = pwd_input();
-    } else if (argc == 3) {
-
+    } else if (argc == 2) {
+        user=std::string(argv[1]);
+        pwd = pwd_input();
     }
+
     au.auth(user, pwd);
     return 0;
 }
