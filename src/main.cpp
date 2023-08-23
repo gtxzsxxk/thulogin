30cf439757a32357682135793ecb5c4b89b34694
hanyuan <2524395907@qq.com>
2023-08-23 10:02:01 +0800

    fix: the bug of unknown info in parameters

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,17 @@ std::string server = "http://auth4.tsinghua.edu.cn";
 std::string user, pwd;
 std::string user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)";
 
-void load_parameters(int &ptr, char **argv);
+int load_parameters(int &ptr, char **argv);
 
 int main(int argc, char **argv) {
     argc_value = argc;
     if (argc > 1) {
         int ptr = 1;
         while (ptr < argc) {
-            load_parameters(ptr, argv);
+            if (load_parameters(ptr, argv)) {
+                std::cerr << "Aborted in parameters parse." << std::endl;
+                return -1;
+            }
         }
     }
     if (username_acquired) {
@@ -48,7 +51,7 @@ int main(int argc, char **argv) {
 }
 
 int argv_param_validate(int &ptr, char **argv) {
-    if (ptr + 1 > argc_value) {
+    if (ptr + 1 >= argc_value) {
         return -1;
     }
     if (strlen(argv[ptr + 1]) > 2) {
@@ -59,7 +62,7 @@ int argv_param_validate(int &ptr, char **argv) {
     return 0;
 }
 
-void load_parameters(int &ptr, char **argv) {
+int load_parameters(int &ptr, char **argv) {
     if (!strcmp(argv[ptr], "--wifi")) {
         ac_id = "135";
     } else if (!strcmp(argv[ptr], "--ethernet")) {
@@ -68,31 +71,49 @@ void load_parameters(int &ptr, char **argv) {
         if (!argv_param_validate(ptr, argv)) {
             user = argv[++ptr];
             username_acquired = 0;
-            return;
+            return 0;
+        } else {
+            std::cerr << "Invalid input near '--username'" << std::endl;
+            return -1;
         }
     } else if (!strcmp(argv[ptr], "--pwd")) {
         if (!argv_param_validate(ptr, argv)) {
             pwd = argv[++ptr];
             password_acquired = 0;
-            return;
+            return 0;
+        } else {
+            std::cerr << "Invalid input near '--pwd'" << std::endl;
+            return -1;
         }
     } else if (!strcmp(argv[ptr], "--server")) {
         if (!argv_param_validate(ptr, argv)) {
             /* TODO: validate the url */
             server = argv[++ptr];
-            return;
+            return 0;
+        } else {
+            std::cerr << "Invalid input near '--server'" << std::endl;
+            return -1;
         }
     } else if (!strcmp(argv[ptr], "--ua")) {
         if (!argv_param_validate(ptr, argv)) {
             user_agent = argv[++ptr];
-            return;
+            return 0;
+        } else {
+            std::cerr << "Invalid input near '--ua'" << std::endl;
+            return -1;
         }
-
     } else if (!strcmp(argv[ptr], "--ac-id")) {
         if (!argv_param_validate(ptr, argv)) {
             ac_id = argv[++ptr];
-            return;
+            return 0;
+        } else {
+            std::cerr << "Invalid input near '--ac-id'" << std::endl;
+            return -1;
         }
+    } else {
+        std::cerr << "Unknown parameter: " << argv[ptr] << std::endl;
+        return -1;
     }
     ptr++;
+    return 0;
 }
